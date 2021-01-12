c1d67fa026aa5ec012db696ffaf2f55d1b08c767
witaaldi <wita.aldi21@gmail.com>
2021-01-13 07:37:38 +0800

    v.2.2 edit router to hashrouter

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 //components
@@ -18,6 +18,7 @@ import fs from "../../assets/icons/free-delivery.svg";
 import ActionType from "../../redux/actions/actionType";
 
 const ProductDetail = () => {
+  let history = useHistory();
   const [isLoaded, setIsLoaded] = useState(false);
   let { itemID } = useParams();
   const stringID = itemID.replace(/_/g, " ");
@@ -88,85 +89,89 @@ const ProductDetail = () => {
     }
   };
 
-  if (isLoaded) {
-    return (
-      <section>
-        <div className="container">
-          <div className="p-detail">
-            <div className="p-images">
-              <div className="p-big-img">
-                <img src={images[itemID]} alt="Shoes" />
-              </div>
-              <ul className="p-small-imgs">
-                <li>
+  if (itemID !== "") {
+    if (isLoaded) {
+      return (
+        <section>
+          <div className="container">
+            <div className="p-detail">
+              <div className="p-images">
+                <div className="p-big-img">
                   <img src={images[itemID]} alt="Shoes" />
-                </li>
-                <li>
-                  <img src={images[itemID]} alt="Shoes" />
-                </li>
-                <li>
-                  <img src={images[itemID]} alt="Shoes" />
-                </li>
-                <li>
-                  <img src={images[itemID]} alt="Shoes" />
-                </li>
-              </ul>
-            </div>
-            <div className="p-desc">
-              <h2 className="p-category">{item.category}</h2>
-              <h1 className="p-name">{item.name}</h1>
-              <p className="p-about">{item.description}</p>
-              <div className="p-video" onClick={() => modal.current.open()}>
-                <div className="play-btn">
-                  <img src={playIcon} alt="Play Video" />
                 </div>
-                <p>PLAY VIDEO</p>
+                <ul className="p-small-imgs">
+                  <li>
+                    <img src={images[itemID]} alt="Shoes" />
+                  </li>
+                  <li>
+                    <img src={images[itemID]} alt="Shoes" />
+                  </li>
+                  <li>
+                    <img src={images[itemID]} alt="Shoes" />
+                  </li>
+                  <li>
+                    <img src={images[itemID]} alt="Shoes" />
+                  </li>
+                </ul>
               </div>
-              <div className="p-size">
-                <p>SELECT SIZE(US)</p>
-                <div className="size-selector">{sizeToRender}</div>
+              <div className="p-desc">
+                <h2 className="p-category">{item.category}</h2>
+                <h1 className="p-name">{item.name}</h1>
+                <p className="p-about">{item.description}</p>
+                <div className="p-video" onClick={() => modal.current.open()}>
+                  <div className="play-btn">
+                    <img src={playIcon} alt="Play Video" />
+                  </div>
+                  <p>PLAY VIDEO</p>
+                </div>
+                <div className="p-size">
+                  <p>SELECT SIZE(US)</p>
+                  <div className="size-selector">{sizeToRender}</div>
+                </div>
+                <div className="p-color">
+                  <p>SELECT COLOR</p>
+                  <div className="color-selector">{colorToRender}</div>
+                </div>
               </div>
-              <div className="p-color">
-                <p>SELECT COLOR</p>
-                <div className="color-selector">{colorToRender}</div>
+            </div>
+            <div className="add-to-bag">
+              <div className="fs-note left">
+                <img src={fs} alt="Free Shipping" />
+                free shipping over $100 purchase
+              </div>
+              <div
+                className="add-btn"
+                onClick={() => addToCart(item.name, item.price, color, size)}
+              >
+                <p>add to bag — ${item.price}</p>
+                <img src={arrow} alt="Add to Bag" />
               </div>
             </div>
           </div>
-          <div className="add-to-bag">
-            <div className="fs-note left">
-              <img src={fs} alt="Free Shipping" />
-              free shipping over $100 purchase
+          <Modal ref={modal}>
+            <div className="video-container">
+              <iframe
+                title={itemID}
+                width="640"
+                height="360"
+                src={item.video}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
-            <div
-              className="add-btn"
-              onClick={() => addToCart(item.name, item.price, color, size)}
-            >
-              <p>add to bag — ${item.price}</p>
-              <img src={arrow} alt="Add to Bag" />
-            </div>
-          </div>
+          </Modal>
+        </section>
+      );
+    } else {
+      return (
+        <div className="loader-container">
+          <div className="loader"></div>
         </div>
-        <Modal ref={modal}>
-          <div className="video-container">
-            <iframe
-              title={itemID}
-              width="640"
-              height="360"
-              src={item.video}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </Modal>
-      </section>
-    );
+      );
+    }
   } else {
-    return (
-      <div className="loader-container">
-        <div className="loader"></div>
-      </div>
-    );
+    history.push(`/product-detail/${id}`);
   }
 };
 
